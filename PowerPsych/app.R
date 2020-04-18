6cd3f5b52011a0ef8452f1a7bf1a6278682fc1af
Aaron Lim <aronlimjm@gmail.com>
2020-04-18 22:57:36 +0800

    WIP

--- PowerPsych/app.R
+++ PowerPsych/app.R
@@ -3,125 +3,91 @@ library(semTools)
 library(car)
 library(DT)
 
-anova1way <- function(n, means, sds, num.grp) {
+anova1way <- function(n, means, sds, num.grp){
+    
+    # simulate data for each group
     for (r in 1:num.grp) {
-        assign(paste0("grp",
-                      r,
-                      ".model"),
-               paste0("y ~ ",
-                      means[r],
-                      "*1",
-                      "\n y ~~ ",
-                      sds[r] ^ 2,
-                      "*y"))
+        # create the model formula to be used in simulateData(model = ...)
+        # assign the model formula to an object called grp*.model,
+        # where * is 1 for group 1 and so on...
+        assign(paste0("grp", r, ".model"),
+               paste0("y ~ ", means[r], "*1", "\n y ~~ ", sds[r] ^ 2, "*y"))
         
-        assign(paste0("data.g",
-                      r),
+        # run simulateData() and save the simulated dataset into an object called 
+        # data.g*, where * is 1 for group 1 and so on...
+        assign(paste0("data.g", r),
                simulateData(eval(parse(
-                   text = paste0("grp",
-                                 r,
-                                 ".model")
+                   text = paste0("grp", r, ".model")
                )),
                sample.nobs = n))
     }
     
-    # retrieve all objects in environment with the suffix data.g
+    # retrieve the names of all objects in the environment with the suffix data.g
     alldata.obs <- ls(pattern = "^data.g")
-    # create formula to create the overall dataframe
+    # create command needed to create the overall dataframe, consisting of data from each group
+    # alldat.formula would take the form of "data.g1[,1], data.g2[,2]"
     alldat.formula <- paste0(alldata.obs, "[,1]", collapse = ",")
+    # wrap alldat.formula in c( and ) to complete the command
     alldat.formula <- paste0("c(", alldat.formula, ")")
     
+    # create a vector X to be used as a dummy variable to indicate the groups of
+    # each row of the results in alldat, created later on...
     X <- c()
     for (r in 1:num.grp) {
         X <- append(X, rep(paste(r), n))
     }
     
+    # alldat is the overall dataframe that contains the data of all the groups
     alldat <- data.frame(Y = eval(parse(text = alldat.formula)),
                          X1 = X)
-    
+    # save the p-value of the main effect from the 1-WAY ANOVA
     anova.results <- Anova(lm(Y ~ X1,
-                              data = alldat))[1, 4]
+                              data = alldat))[1,4]
     
+    # if the p value is statistically significant, let sig = 1,
+    # else sig = 0
     if (anova.results < 0.05) {
         sig <- 1
     } else {
         sig <- 0
     }
     
+    # the output of the function is either 0 or 1, 
+    # 0 = main effect was not statistically significant,
+    # 1 = main effect was statistically significant
     return(sig)
-    counter <- counter + 1
-    print(counter)
 }
-
-# progress "bar"
-progress <- 0
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    sidebarLayout(
-        sidebarPanel(
-            actionButton(
-                "update",
-                label = "Calculate"
-            ),
-            selectInput(
-                "alys",
-                label = h3("Analysis"),
-                choices = list(
-                    "One-Way ANOVA" = "ANV1",
-                    "Two-Way ANOVA" = "ANV2",
-                    "Multiple Regression" = "MR",
-                    "Multiple Regression With Interaction" = "MRWI"
-                ),
-                selected = "None"
-            ),
-            numericInput(
-                "numIV",
-                label = h3("Number Of Independent Variables"),
-                value = 1
-            ),
-            numericInput(
-                "sampsize",
-                label = h3("Sample Size Per Group"),
-                value = 100
-            ),
-            numericInput(
-                "g1mean",
-                label = h3("Mean Of Group 1"),
-                value = 1
-            ),
-            numericInput(
-                "g2mean",
-                label = h3("Mean Of Group 2"),
-                value = -1
-            ),
-            numericInput(
-                "g1sd",
-                label = h3("SD Of Group 1"),
-                value = 1
-            ),
-            numericInput(
-                "g2sd",
-                label = h3("SD Of Group 2"),
-                value = 1
-            ),
-            numericInput(
-                "iter",
-                label = h3("Number Of Iterations"),
-                value = 100
-            )
-        ),
-        mainPanel(
-            dataTableOutput(outputId = "power")
-        )
-    ))
+    title = "One-Way ANOVA",
+    fluidRow(
+        column(2,
+               numericInput("sampsize", label = h3("Sample Size Per Group"), value = 100),
+               numericInput("iter",label = h3("Number Of Iterations"), value = 100)
+               ),
+        column(2,
+               numericInput("g1mean", label = h3("Mean Of Group 1"), value = 1),
+               numericInput("g1sd", label = h3("SD Of Group 1"), value = 1),
+               ),
+        column(2,
+               numericInput("g2mean",label = h3("Mean Of Group 2"), value = -1),
+               numericInput("g2sd", label = h3("SD Of Group 2"), value = 1),
+               actionButton("update", label = "Calculate"))
+    ),
+    hr(),
+    column(verbatimTextOutput(outputId = "power"), offset = 4, width = 4)
+)
+
 
 server <- function(input, output) {
     
-    # tracker
+    # tracker is the object which will keep track of how many time the
+    # main effect of X1 was statistically significant
     tracker <- data.frame(MainEffect = c("X1"),
                           Count = c(0))
-    
+    # create params, a list of reactive values that will create the interactivity of
+    # the app
     params <- reactiveValues(sampsize = 100,
                              g1mean = 1,
                              g2mean = 2,
@@ -129,6 +95,8 @@ server <- function(input, output) {
                              g2sd = 1,
                              iter = 100)
     
+    # ensure that the results only update when the calculate
+    # button is pressed and input$update is invalidated
     observeEvent(input$update,{
         
         params$sampsize <- input$sampsize
@@ -139,21 +107,22 @@ server <- function(input, output) {
         params$iter <- input$iter
     })
     
-    output$power <- renderDataTable({
+    # the main output, which is a dataframe but looks nicer when printed
+    output$power <- renderPrint({
         
-        # loop
+        # a progress counter
+        progress <- 0
+        
+        # loop repeating anova1way() with appropriate parameters for the number of
+        # iterations specified by the user
         for (i in 1:params$iter){
             tracker$Count[1] <- tracker$Count[1] + anova1way(n = params$sampsize, 
                                                              means = c(params$g1mean, params$g2mean), 
                                                              sds = c(params$g1sd, params$g2sd), 
                                                              num.grp = 2)
             progress <- progress + 1
-            # print(paste0(progress,
-            #              " out of ",
-            #              iterations,
-            #              " iterations completed!"))
+            # print(paste0(progress, " out of ", params$iter, " iterations completed!"))
         }
-        
         tracker
     })
 }
