7616376a840924c29b2e4c65ab6ecbe2071b905d
Aaron Lim <aronlimjm@gmail.com>
2020-04-19 20:28:57 +0800

    8/10 DONE 1 WAY.

--- PowerPsych/app.R
+++ PowerPsych/app.R
@@ -2,8 +2,9 @@ library(shiny)
 library(semTools)
 library(car)
 library(DT)
+library(shinythemes)
 
-anova1way <- function(n, means, sds, num.grp){
+anova1way <- function(means, sds, grpsize, num.grp){
     
     # simulate data for each group
     for (r in 1:num.grp) {
@@ -19,7 +20,8 @@ anova1way <- function(n, means, sds, num.grp){
                simulateData(eval(parse(
                    text = paste0("grp", r, ".model")
                )),
-               sample.nobs = n))
+               sample.nobs = grpsize[r]
+               ))
     }
     
     # retrieve the names of all objects in the environment with the suffix data.g
@@ -34,7 +36,7 @@ anova1way <- function(n, means, sds, num.grp){
     # each row of the results in alldat, created later on...
     X <- c()
     for (r in 1:num.grp) {
-        X <- append(X, rep(paste(r), n))
+        X <- append(X, rep(paste(r), grpsize[r]))
     }
     
     # alldat is the overall dataframe that contains the data of all the groups
@@ -60,21 +62,31 @@ anova1way <- function(n, means, sds, num.grp){
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    title = "One-Way ANOVA",
+    theme = shinytheme("flatly"),
+    titlePanel(h1("One-Way ANOVA Power Simulator")),
     fluidRow(
         column(4,
                wellPanel(
-                   numericInput("sampsize", label = h3("Sample Size Per Group"), value = 100),
+                   h3("Please begin by selecting the general settings within this panel."),
                    numericInput("iter",label = h3("Number Of Iterations"), value = 100),
+                   "The option above determines the number of iteration for the simulation, please note that selecting larger number of iterations will increase computational time.",
+                   numericInput("seed", label = h3("Set Seed For Replicability"), value = 123456),
+                   "The seed ensures that the simulation is replicable, it is ideal to record down the seed utilized. Any sufficiently-large number can be used for the seed.",
                    sliderInput("numgrps", label = h3("Number Of Groups"), min = 2, max = 9, value = 2),
-                   actionButton("update", label = "Calculate")
+                   "The number of levels your independent variable possesses. Or the number of groups you have in your design.",
+                   hr(),
+                   actionButton("update", label = "Run!"),
+                   hr(),
+                   "Click Run! when you are satisfied with your settings and are ready to begin the simulation. The output will appear below, the application may take a while so hang tight!"
                )),
-    ),
-    hr(),
-    fluidRow(
         column(wellPanel(h3("Group Means"), uiOutput("grpmeans")), width = 2),
         column(wellPanel(h3("Group SDs"), uiOutput("grpsds")), width = 2),
-        column(verbatimTextOutput(outputId = "power"), offset = 0, width = 6),
+        column(wellPanel(h3("Group Size"), uiOutput("grpsize")), width = 2)
+    ),
+    hr(),
+    titlePanel("Output"),
+    fluidRow(
+                column(verbatimTextOutput(outputId = "power"), offset = 0, width = 6),
     )
 )
 
@@ -99,11 +111,21 @@ server <- function(input, output) {
         })
     })
     
+    output$grpsize <- renderUI({
+        numgrps <- as.integer(input$numgrps)
+        lapply(1:numgrps, function(i) {
+            numericInput(paste0("g",i,"size"),
+                         label = paste0("Size Of Group ", i),
+                         value = 50)
+        })
+    })
+    
     
     # tracker is the object which will keep track of how many time the
     # main effect of X1 was statistically significant
-    tracker <- data.frame(MainEffect = c("X1"),
-                          Count = c(0))
+    tracker <- data.frame(NumIterations = 0,
+                          NumSig = c(0)
+                          )
     # create params, a list of reactive values that will create the interactivity of
     # the app
     params <- reactiveValues(numgrps = 2)
@@ -113,15 +135,17 @@ server <- function(input, output) {
     # browser()
     observeEvent(input$update,{
     
-        params$sampsize <- input$sampsize
         params$iter <- input$iter
         params$numgrps <- input$numgrps
+        params$seed <- input$seed
         
         if(input$numgrps == 2){
             params$g1mean <- input$g1mean
             params$g2mean <- input$g2mean
             params$g1sd <- input$g1sd
             params$g2sd <- input$g2sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
         }
         
         if(input$numgrps == 3){
@@ -131,6 +155,9 @@ server <- function(input, output) {
             params$g1sd <- input$g1sd
             params$g2sd <- input$g2sd
             params$g3sd <- input$g3sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
         }
         
         if(input$numgrps == 4){
@@ -142,6 +169,10 @@ server <- function(input, output) {
             params$g2sd <- input$g2sd
             params$g3sd <- input$g3sd
             params$g4sd <- input$g4sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
+            params$g4size <- input$g4size
         }
         
         if(input$numgrps == 5){
@@ -155,6 +186,11 @@ server <- function(input, output) {
             params$g3sd <- input$g3sd
             params$g4sd <- input$g4sd
             params$g5sd <- input$g5sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
+            params$g4size <- input$g4size
+            params$g5size <- input$g5size
         }
         
         if(input$numgrps == 6){
@@ -170,6 +206,12 @@ server <- function(input, output) {
             params$g4sd <- input$g4sd
             params$g5sd <- input$g5sd
             params$g6sd <- input$g6sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
+            params$g4size <- input$g4size
+            params$g5size <- input$g5size
+            params$g6size <- input$g6size
             
         }
         
@@ -188,6 +230,13 @@ server <- function(input, output) {
             params$g5sd <- input$g5sd
             params$g6sd <- input$g6sd
             params$g7sd <- input$g7sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
+            params$g4size <- input$g4size
+            params$g5size <- input$g5size
+            params$g6size <- input$g6size
+            params$g7size <- input$g7size
         }
         
         if(input$numgrps == 8){
@@ -207,6 +256,14 @@ server <- function(input, output) {
             params$g6sd <- input$g6sd
             params$g7sd <- input$g7sd
             params$g8sd <- input$g8sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
+            params$g4size <- input$g4size
+            params$g5size <- input$g5size
+            params$g6size <- input$g6size
+            params$g7size <- input$g7size
+            params$g8size <- input$g8size
         }
         
         if(input$numgrps == 9){
@@ -228,6 +285,15 @@ server <- function(input, output) {
             params$g7sd <- input$g7sd
             params$g8sd <- input$g8sd
             params$g9sd <- input$g9sd
+            params$g1size <- input$g1size
+            params$g2size <- input$g2size
+            params$g3size <- input$g3size
+            params$g4size <- input$g4size
+            params$g5size <- input$g5size
+            params$g6size <- input$g6size
+            params$g7size <- input$g7size
+            params$g8size <- input$g8size
+            params$g9size <- input$g9size
         }
         
     })
@@ -242,6 +308,8 @@ server <- function(input, output) {
         meanform <- paste0("c(", meanform, ")")
         sdform <- paste0("params$g", 1:params$numgrps, "sd", collapse = ",")
         sdform <- paste0("c(", sdform, ")")
+        sizeform <- paste0("params$g", 1:params$numgrps, "size", collapse = ",")
+        sizeform <- paste0("c(", sizeform, ")")
         
         if(input$update == 0){
             print("Please input parameters and press the Calculate button.")
@@ -249,15 +317,18 @@ server <- function(input, output) {
 
             # loop repeating anova1way() with appropriate parameters for the number of
             # iterations specified by the user    
+            set.seed(params$seed)
             for(i in 1:params$iter){
-                    tracker$Count[1] <- tracker$Count[1] + anova1way(n = params$sampsize, 
-                                                                     means = eval(parse(text = meanform)), 
+                    tracker$NumSig[1] <- tracker$NumSig[1] + anova1way(means = eval(parse(text = meanform)), 
                                                                      sds = eval(parse(text = sdform)), 
+                                                                     grpsize = eval(parse(text = sizeform)),
                                                                      num.grp = params$numgrps)
                     # progress <- progress + 1
                     # print(paste0(progress, " out of ", params$iter, " iterations completed!"))
-                }
-
+            }
+            tracker$NumIterations <- params$iter
+            rownames(tracker) <- "Main Effect"
+            tracker$Power <- tracker$NumSig / tracker$NumIterations
             tracker
         }
     })
